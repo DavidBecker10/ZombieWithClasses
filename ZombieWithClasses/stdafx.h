bf31a22b7e36247eb3acacd9d64e70985b844d67
David Becker <davidsouza@alunos.utfpr.edu.br>
2021-11-23 13:28:06 -0300

    assets

--- ZombieWithClasses/stdafx.h
+++ ZombieWithClasses/stdafx.h
@@ -8,14 +8,14 @@
 using std::cout;
 using std::endl;
 
-#define FONT_PATH "../Sprites/Font/SIXTY.TTF"
-#define HOMER_PATH "../Sprites/Zombies/Homer/Attack1.png"
-#define GHOUL_PATH "../Sprites/Zombies/Ghoul/Attack1.png"
-#define NEMESIS_PATH "../Sprites/Zombies/Nemesis/Attack1.png"
+#define FONT_PATH "../Assets/Font/SIXTY.TTF"
+#define HOMER_PATH "../Assets/Zombies/Homer/Attack1.png"
+#define GHOUL_PATH "../Assets/Zombies/Ghoul/Attack1.png"
+#define NEMESIS_PATH "../Assets/Zombies/Nemesis/Attack1.png"
 /**--------------------------------------------------------------------------------------------------------------------*/
-#define BULLET_PATH "../Sprites/Projectiles/bullet.png"
-#define BONE_PATH "../Sprites/Projectiles/bone.png"
+#define BULLET_PATH "../Assets/Projectiles/bullet.png"
+#define BONE_PATH "../Assets/Projectiles/bone.png"
 
-#define RACOON_PATH "../Sprites/Maps/mapStage1.json"
-#define PLAYER_PATH "../Sprites/Terrorists/Masked/Attack1_2.png"
-#define PLAYER2_PATH "../Sprites/Terrorists/Muslim/Attack1_1.png"
+#define RACOON_PATH "../Assets/Maps/mapStage1.json"
+#define PLAYER_PATH "../Assets/Terrorists/Masked/Attack1_2.png"
+#define PLAYER2_PATH "../Assets/Terrorists/Muslim/Attack1_1.png"
